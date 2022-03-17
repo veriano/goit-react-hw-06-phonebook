04d1402098e1bae0edb345ecb31178c40eb2c272
valeriy <veriano@ukr.net>
2022-03-17 15:44:02 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  App from './components/App';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+reportWebVitals();
--- src/redux/action-types.js
+++ src/redux/action-types.js
@@ -2,4 +2,5 @@ const ADD_CONTACT = 'addContact';
 const DELETE_CONTACT = 'deleteContact';
 const FILTER = 'filter';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { ADD_CONTACT, DELETE_CONTACT, FILTER };
