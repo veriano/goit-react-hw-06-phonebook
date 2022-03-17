00e9fff3917f296b6c2e9a0fbce7a540ee63996a
valeriy <veriano@ukr.net>
2022-03-17 19:20:03 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Form from 'Components/form/Form';
-import Filter from './Components/filter/Filter';
-import ContactList from './Components/contacts-list/ContactList';
+import Filter from 'Components/filter/Filter';
+import ContactList from 'Components/contacts-list/ContactList';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { store } from './redux/store';
